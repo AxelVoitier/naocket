90ef21ae06783c0af1e3c2148cff4d110072a1b4
Axel Voitier <axel.voitier@gmail.com>
2013-05-24 14:58:13 +0200

    Working! Was missing json accept header.

--- pocket/api.py
+++ pocket/api.py
@@ -3,6 +3,6 @@ import json
 
 def get_request_token(consumer_key, redirect_uri):
     data = {'consumer_key': consumer_key, 'redirect_uri': redirect_uri}
-    headers = {'content-type': 'application/json; charset=UTF-8'}
+    headers = {'Content-Type': 'application/json; charset=UTF-8', 'X-Accept': 'application/json'}
     response = requests.post('https://getpocket.com/v3/oauth/request', data=json.dumps(data), headers=headers)
-    return response
+    return response.json()['code']
