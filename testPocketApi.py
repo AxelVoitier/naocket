624336c8ae628af92289b870d85ef9b0399d055e
Axel Voitier <axel.voitier@gmail.com>
2013-05-24 14:43:43 +0200

    Having the consumer key in a separate python file.

--- testPocketApi.py
+++ testPocketApi.py
@@ -1,5 +1,4 @@
 from pocket import api as pocket
+import pocketConsumerKey
 
-POCKET_PLATFORM_CONSUMER_KEY = ''
-
-print pocket.get_request_token(POCKET_PLATFORM_CONSUMER_KEY, 'pocketapp1234:authorizationFinished')
+print pocket.get_request_token(pocketConsumerKey.key, 'pocketapp1234:authorizationFinished')
