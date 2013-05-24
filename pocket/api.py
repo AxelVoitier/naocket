f297b6c3f8d6fdcfb9b2c0a5ab25ed519ceb8d56
Axel Voitier <axel.voitier@gmail.com>
2013-05-24 18:50:35 +0200

    Moved the API in its own class.

--- pocket/api.py
+++ pocket/api.py
@@ -3,7 +3,6 @@ from requests.exceptions import HTTPError
 import json
 
 class PocketException(Exception):
-    
     def __init__(self, errCode, errMessage):
         self.errCode = int(errCode)
         self.errMessage = errMessage
@@ -23,36 +22,52 @@ class PocketAccessDeniedException(PocketException):
 class PocketServerDownException(PocketException):
     pass
     
-def callToPocketAPI(url, data):
-    headers = {'Content-Type': 'application/json; charset=UTF-8', 'X-Accept': 'application/json'}
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    try:
-        response.raise_for_status()
-        limits = filter(lambda x: x[0].startswith('x-limit-'), response.headers.items())
-        return (response.json(), limits)
-    except HTTPError:
-        if response.status_code == 400:
-            raise PocketInvalidRequestException(response.headers['x-error-code'], response.headers['x-error'])
-        elif response.status_code == 401:
-            raise PocketUserAuthenticationException(response.headers['x-error-code'], response.headers['x-error'])
-        elif response.status_code == 403:
-            raise PocketAccessDeniedException(response.headers['x-error-code'], response.headers['x-error'])
-        elif response.status_code == 503:
-            raise PocketServerDownException(response.headers['x-error-code'], response.headers['x-error'])
-        else:
-            raise PocketException(response.headers['x-error-code'], response.headers['x-error'])
-    
+class PocketAPI(object):
 
-def getRequestToken(consumerKey, redirectUri):
-    data = {'consumer_key': consumerKey, 'redirect_uri': redirectUri}
-    return callToPocketAPI('https://getpocket.com/v3/oauth/request', data)[0]['code']
-            
-def getAccessToken(consumerKey, code):
-    data = {'consumer_key': consumerKey, 'code': code}
-    response = callToPocketAPI('https://getpocket.com/v3/oauth/authorize', data)
-    return (response['access_token'], response['username'])[0]
+    def __init__(self, consumerKey, accessToken=None, username=None):
+        self.consumerKey = consumerKey
+        self.accessToken = accessToken
+        self.username = username
+        self.limits = None
+        
+    def _rawCallToPocketAPI(self, url, data):
+        headers = {'Content-Type': 'application/json; charset=UTF-8', 'X-Accept': 'application/json'}
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        try:
+            response.raise_for_status()
+            limits = filter(lambda x: x[0].startswith('x-limit-'), response.headers.items())
+            if (limits is not None) and (len(limits) > 0):
+                self.limits = limits
+                
+            return response.json()
+        except HTTPError:
+            if response.status_code == 400:
+                raise PocketInvalidRequestException(response.headers['x-error-code'], response.headers['x-error'])
+            elif response.status_code == 401:
+                raise PocketUserAuthenticationException(response.headers['x-error-code'], response.headers['x-error'])
+            elif response.status_code == 403:
+                raise PocketAccessDeniedException(response.headers['x-error-code'], response.headers['x-error'])
+            elif response.status_code == 503:
+                raise PocketServerDownException(response.headers['x-error-code'], response.headers['x-error'])
+            else:
+                raise PocketException(response.headers['x-error-code'], response.headers['x-error'])
     
-def retrieve(consumerKey, accessToken):
-    data = {'consumer_key': consumerKey, 'access_token': accessToken}
-    response = callToPocketAPI('https://getpocket.com/v3/get', data)
-    return response
+    def _authCallToPocketAPI(self, url, data):
+        data['consumer_key'] = self.consumerKey
+        data['access_token'] = self.accessToken
+        return self._rawCallToPocketAPI(url, data)
+
+    def getRequestToken(self, redirectUri):
+        data = {'consumer_key': self.consumerKey, 'redirect_uri': redirectUri}
+        return self._rawCallToPocketAPI('https://getpocket.com/v3/oauth/request', data)['code']
+            
+    def getAccessToken(self, code):
+        data = {'consumer_key': self.consumerKey, 'code': code}
+        response = self._rawCallToPocketAPI('https://getpocket.com/v3/oauth/authorize', data)
+        self.accessToken = response['access_token']
+        self.username = response['username']
+        return (self.accessToken, self.username)
+    
+    def retrieve(self, data = {}):
+        response = self._authCallToPocketAPI('https://getpocket.com/v3/get', data)
+        return response
--- testPocketApi.py
+++ testPocketApi.py
@@ -2,14 +2,17 @@ from pocket import api as pocket
 import pocketConsumerKey
 
 if 'accessToken' not in dir(pocketConsumerKey):
+    api = pocket.PocketAPI(pocketConsumerKey.key)
     redirectUri = 'pocketapp1234:authorizationFinished'
-    reqCode = pocket.getRequestToken(pocketConsumerKey.key, redirectUri)
+    reqCode = api.getRequestToken(redirectUri)
 
     print 'https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s' % (reqCode, redirectUri)
 
     raw_input('Press enter')
 
-    pocketConsumerKey.accessToken = pocket.getAccessToken(pocketConsumerKey.key, reqCode)
+    pocketConsumerKey.accessToken = api.getAccessToken(reqCode)
     print pocketConsumerKey.accessToken
+else:
+    api = pocket.PocketAPI(pocketConsumerKey.key, pocketConsumerKey.accessToken[0], pocketConsumerKey.accessToken[1])
     
-print pocket.retrieve(pocketConsumerKey.key, pocketConsumerKey.accessToken[0])
+print api.retrieve()
